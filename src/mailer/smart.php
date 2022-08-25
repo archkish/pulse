59cce97937c029758a4d19aae8ef3201506eeb07
arch <archkish@gmail.com>
2022-08-25 23:01:35 +0200

    mailer

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'kisharchi@gmail.com';                 // Наш логин
-$mail->Password = 'qanxqgzoushnizbr';                           // Наш пароль от ящика
+$mail->Password = 'amcclusiecxfaply';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
